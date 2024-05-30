15ed4bf82ece1aa6f97332011d33d0d6626225e0
JARED MATAYO <77388302+jaredmatayo2000@users.noreply.github.com>
2024-05-30 11:39:58 +0300

    Update homesreen.js

--- src/components/homesreen.js
+++ src/components/homesreen.js
@@ -9,14 +9,14 @@ export default class Home extends Component{
 
     <div className=" jumbotron mt-5 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">ONLINE DRUG MANAGEMENT SYSTEM</h1>
+        <h1 className="display-5 fw-bold">USERNAME LAND LISTING</h1>
         <hr className="my-4" />
         <p className="col-md-8 fs-4">
-          Find affordable drugs here on our website...
+          Find affordable Lands here on our website...
         </p>
-        <Link className="btn btn-primary btn-lg" to="/drugs" role="button">
+        <Link className="btn btn-primary btn-lg" to="/lands" role="button">
             
-            Click to choose your drug here...
+            Click to choose your land here...
             </Link>
         
       </div>
